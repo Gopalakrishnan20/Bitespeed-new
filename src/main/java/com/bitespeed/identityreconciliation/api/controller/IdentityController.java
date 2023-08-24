b63e92c9c33434f027bcf4ae58762adae835ff5e
Gopalakrishnan20 <gopalakrishnan567890@gmail.com>
2023-08-24 22:07:40 +0530

    New changes

---
+++ src/main/java/com/bitespeed/identityreconciliation/api/controller/IdentityController.java
@@ -0,0 +1,37 @@
+package com.bitespeed.identityreconciliation.api.controller;
+
+import com.bitespeed.identityreconciliation.api.entity.Contact;
+import com.bitespeed.identityreconciliation.api.model.IdentityRequest;
+import com.bitespeed.identityreconciliation.api.model.IdentityResponse;
+import com.bitespeed.identityreconciliation.api.service.IdentityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class IdentityController {
+	
+	@Autowired
+    private IdentityService identityService;
+    /**
+     * Endpoint to perform identity reconciliation based on the provided JSON payload.
+     * The request body should contain either an email or phoneNumber for identification.
+     *
+     * @param request The JSON request containing email or phoneNumber for identification.
+     * @return ResponseEntity with IdentityResponse containing the consolidated contact details.
+     */
+    @PostMapping("/identify")
+    public ResponseEntity<IdentityResponse> identifyContact(@RequestBody IdentityRequest request) {
+        // Delegate the identity reconciliation logic to the service
+        IdentityResponse response = identityService.identifyContact(request);
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("/xyz")
+    public List<Contact> all(){
+        return identityService.all();
+    }
+}
+
---
+++ src/main/java/com/bitespeed/identityreconciliation/api/repository/ContactRepository.java
@@ -0,0 +1,24 @@
+package com.bitespeed.identityreconciliation.api.repository;
+
+import com.bitespeed.identityreconciliation.api.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+	
+	Optional<Contact> findById(Long id);
+    
+	 List<Contact> findByEmail(String email);
+
+	 List<Contact> findByPhoneNumber(String phoneNumber);
+	 
+	 @Query("SELECT c FROM Contact c WHERE c.phoneNumber = :phoneNumber AND c.email = :email")
+	 Optional<Contact> findByPhoneNumberAndEmail(String phoneNumber, String email);
+	 
+	 List<Contact> findByLinkedId(Long id);
+}
